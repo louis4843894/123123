dbb052440f083e896327f93da1879cc12ef321ef
louis <cvu31161a@gmail.com>
2026-05-02 22:58:51 +0800

    刪除

--- components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-import { getSession } from '@/lib/auth'
-import { NavbarClient } from './navbar-client'
-import { getSettings } from '@/lib/db'
-
-export async function Navbar() {
-  const session = await getSession()
-  const settings = getSettings()
-  return <NavbarClient session={session} phone={settings.phone} />
-}
